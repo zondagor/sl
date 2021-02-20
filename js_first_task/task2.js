d2af5dde3f216aa44e71d4ea5d8ec287d7f975ee
alex <debryniuk@gmail.com>
2021-02-21 00:11:20 +0200

    regex to be added in filter

--- js_first_task/task2.js
+++ js_first_task/task2.js
@@ -1,7 +1,9 @@
 const mainFunc = (csvText) => {
 	let rating = 1;
+	const topCitiesNames = [];
 
-	const citiesInfo = csvText.split('\n')
+	const topCitiesInfo = csvText
+		.split('\n')
 		.filter((cityStr) => {
 			// todo add regex later
 			if (cityStr.charAt(0) === '#' || cityStr.length === 0) {
@@ -24,21 +26,32 @@ const mainFunc = (csvText) => {
 		.slice(0, 10)
 		.reduce((comprisedCitiesInfo, cityObj) => {
 			const { name: cityName, population } = cityObj;
+			topCitiesNames.push(cityName);
 			comprisedCitiesInfo[cityName] = { population, rating };
 			rating++;
 			return comprisedCitiesInfo;
 		}, {});
 
 	return (textWithCityName) => {
-        return textWithCityName.replace("/a/g", "п")
-    };
+		topCitiesNames.forEach((cityName) => {
+			if (textWithCityName.includes(cityName)) {
+				const { population, rating } = topCitiesInfo[cityName];
+				textWithCityName = textWithCityName.replace(
+					cityName,
+					`${cityName} (население: ${population}, популярность: ${rating})`
+				);
+			}
+		});
+
+		return textWithCityName;
+	};
 };
 
+// todo later check for 30,40,Львов,#300000
 const citiesCsv = `10,20,Кропивницкий,200000
 30,40,Львов,300000
 
 #fdsfsdfsdf
-30,40,Львов,#300000
 50,60,Харьков,500000
 70,80,Одесса,600000
 70,80,Тернополь,100000
@@ -50,4 +63,4 @@ const citiesCsv = `10,20,Кропивницкий,200000
 70,80,Винница,9900000
 70,80,Суммы,50000`;
 
-console.log(mainFunc(citiesCsv)('вчера мы приехали в Киев, завтра едем в...'));
+console.log(mainFunc(citiesCsv)('вчера мы приехали в Луцк, завтра едем в Ужгород'));
