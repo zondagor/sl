8405ff5a75b8ae1ac0ac27e4cbc05036eb42e16e
alex <debryniuk@gmail.com>
2021-02-28 11:20:37 +0200

    data table progress + projects configs added

---
+++ data_table/fff.js
@@ -0,0 +1,85 @@
+let req = {
+	sss: {
+		name: "ddddd",
+		lastName: 'aaaaa',
+		dd: 'xxxx',
+		// ff: { dino: 'aaaaa', ssss: "ssss" },
+		ff: [1,2,4,56],
+	},
+};
+
+let { ff: [firstEl], lastName, dd } = req.sss;
+
+
+// let ffff = [1,4,56,6,7]
+
+// let [,,,, secondEl] = ffff;
+
+// console.log(firstEl)
+// console.log(secondEl)
+
+
+// console.log(name)
+
+// console.log(req)
+
+// console.log(name);
+// console.log(req);
+
+// let name = req.name;
+// let lastName = req.lastName;
+// let dd = req.dd;
+
+
+// console.log(lastName);
+// console.log(dd);
+
+
+// function hook() {
+//     return ["firstEl", () => {console.log("dddd")}]
+// }
+
+// let [first, second] = hook();
+// // let ff = hook();
+// // let first = ff[0]
+// // let second = ff[1]
+// console.log(first)
+// console.log(second)
+
+
+
+let ddd = (a,b) => {
+    return a + b;
+}
+
+// ddd = ddd(2,3)
+
+
+let ddd = 40
+ddd = "dddd"
+
+// ddd.xxx = "ascii"
+
+console.log(ddd.xxx)
+
+
+// let res = ddd("aaa", "dddd")
+// ddd("aaa", "dddd")
+// ddd("aaa", "dddd")
+// ddd("aaa", "dddd")
+// ddd("aaa", "dddd")
+
+
+function aaa() {
+
+}
+
+aaa()
+
+function fff() {
+	
+}
+
+let obj = new fff(2, "sss")
+
+
--- data_table/index.js
+++ data_table/index.js
@@ -1,11 +1,16 @@
 // todo think about what if some params didn't come
-
 async function DataTable(config, data) {
-	let dataArr = data
+	let dataArr = data;
 	if (!dataArr) {
-		let res = await fetch(config.apiUrl)
-		res = await res.json()
-		console.log(res)
+		let res = await fetch(config.apiUrl);
+		res = await res.json();
+		const arrOfObjects = [];
+
+		for (user in res.data) {
+			arrOfObjects.push(res.data[user]);
+		}
+
+		dataArr = arrOfObjects;
 	}
 
 	const divContainer = document.querySelector(config.parent);
@@ -15,6 +20,11 @@ async function DataTable(config, data) {
 	}
 
 	const columnNames = [];
+
+	const addEntityBtn = document.createElement('button');
+	addEntityBtn.innerHTML = 'add new'
+
+	divContainer.appendChild(addEntityBtn);
 
 	const table = document.createElement('table');
 	divContainer.appendChild(table);
@@ -31,11 +41,42 @@ async function DataTable(config, data) {
 		columnNames.push(columns[i].value);
 		tableRowInHeader.appendChild(th);
 	}
+	const actionColumn = document.createElement('th'); // last column in head
+	actionColumn.innerHTML = 'Action';
+	tableRowInHeader.appendChild(actionColumn);
 
 	// table body
 	const tableBody = document.createElement('tbody');
 	table.appendChild(tableBody);
+	
+	const rowWithInputs = document.createElement('tr');
+	const arrWithInputs = [];
+	// rowWithInputs.style.display = 'none';
+	tableBody.appendChild(rowWithInputs);
+	for (columnName of columnNames) {
+		const tableData = document.createElement('td');
+		const input = document.createElement('input');
+		// input.addEventListener("focus", () => {input.style.border = "2px solid red"})
+		input.addEventListener("keydown", (e) => {
+			if (e.key === "Enter"){
+				let allFieldsAreFilled = true;
+				for (let i = 0; i < arrWithInputs.length; i++) {
+					const inputEl = arrWithInputs[i];
+					if (inputEl.value === '') {
+						allFieldsAreFilled = false;
+						inputEl.style.border = "2px solid red"
+					}
+				}
+				
+			}
+		})
+		arrWithInputs.push(input);
+		tableData.appendChild(input);
+		rowWithInputs.appendChild(tableData);
+	}
+
 	for (let i = 0; i < dataArr.length; i++) {
+		// each iteration is a new table row
 		const tableRow = document.createElement('tr');
 		tableBody.appendChild(tableRow);
 
@@ -44,10 +85,15 @@ async function DataTable(config, data) {
 			tableData.innerHTML = dataArr[i][columnName] ? dataArr[i][columnName] : '';
 			tableRow.appendChild(tableData);
 		}
-		
+		const cellForActions = document.createElement('td'); // last cell in the row
+		const deleteButton = document.createElement('button');
+		deleteButton.innerHTML = 'Delete';
+		deleteButton.onclick = () => deleteTableEntity(dataArr, i);
+		cellForActions.appendChild(deleteButton);
+		tableRow.appendChild(cellForActions);
 	}
 
-    return table;
+	return table;
 }
 
 const config1 = {
@@ -57,24 +103,40 @@ const config1 = {
 		{ title: 'Фамилия', value: 'surname' },
 		{ title: 'Возраст', value: 'age' },
 	],
-	apiUrl: "http://mock-api.shpp.me/asadov/users"
 };
 
 const users = [
 	{ id: 30050, name: 'Вася', surname: 'Петров', age: 12 },
 	{ id: 30051, name: 'Вася', surname: 'Васечкин', age: 15 },
-	{ id: 30051, dd: 'Вася', surname: 'Васечкин', age: 17, ff: "fff" },
+	{ id: 30051, dd: 'Вася', surname: 'Васечкин', age: 17, ff: 'fff' },
 ];
 
-
 // DataTable(config1, users);
-DataTable(config1);
 
+const config2 = {
+	parent: '#usersTable',
+	columns: [
+		{ title: 'Имя', value: 'name' },
+		{ title: 'Фамилия', value: 'surname' },
+		{ title: 'ДР=))', value: 'birthday' },
+	],
+	apiUrl: 'http://mock-api.shpp.me/sdebryniuk/users',
+};
 
+function deleteTableEntity(dataArr, i) {
+	const urlForDeletion = `${config2.apiUrl}/${dataArr[i].id}`;
+
+	fetch(urlForDeletion, { method: 'DELETE' })
+		.then((response) => response.json())
+		.then((res) => {
+			document.querySelector('table').remove();
+			DataTable(config2);
+		});
+}
+
+DataTable(config2);
 
 // const table = new DataTable(config1, users);
-
-
 
 // const table = new Tabulator("#usersTable", config1);
 // document.body.appendChild(table);
